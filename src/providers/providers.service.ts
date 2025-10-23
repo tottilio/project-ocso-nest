671e58360408984510aaf0082021e441de3f13be
tottiLap2 <rodrigoguerrerogarcia777futbol@gmail.com>
2025-10-22 20:13:43 -0600

    fixing providers services

--- src/providers/providers.service.ts
+++ src/providers/providers.service.ts
@@ -32,8 +32,13 @@ export class ProvidersService {
   }
 
   findOne(id: string) {
-    return this.providerRepository.findOneBy({
-      providerId: id
+    return this.providerRepository.findOne({
+      where:{
+        providerId: id
+      },
+      relations:{
+        products: true
+      }
     })
   }
 
